18b72b5a852481bdeb2481d8e2770d18d5af1fdb
Moorsknight11 <hajjejhazem063@gmail.com>
2025-05-19 20:29:59 +0200

    Fresh start

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def get_delegations():
     df = pd.DataFrame(data)
     return jsonify(df.to_dict(orient="records"))
 
-@app.route("/", methods=["POST"])
+@app.route("/")
 def index():
     response = requests.get("https://moors.rf.gd/query.php", params={
         "gouvernorats": "all",
@@ -49,7 +49,442 @@ def index():
     gouvernorats = gouvernorats_df.to_dict(orient='records')
     return render_template("index.html", gouvernorats=gouvernorats, selected_code=None)
 
+@app.route("/filter")
+def filter():
+    selected_code = request.form.get("code_gouvernorat")
+    if not selected_code:
+        return redirect(url_for('index'))
 
+    response = requests.get("https://moors.rf.gd/query.php", params={
+        "gouvernorats": "filter",
+        "selected_code":selected_code
+
+    })
+
+    if response.status_code != 200:
+        return "Failed to fetch data", 500
+
+    data = response.json()
+    gouvernorats_df = pd.DataFrame(data)
+    gouvernorats = gouvernorats_df.to_dict(orient='records')
+
+    return render_template("index.html", gouvernorats=gouvernorats, selected_code=selected_code)
+
+def autopct_format(pct, allvals):
+    absolute = int(round(pct / 100. * sum(allvals)))
+    return f"{pct:.1f}%\n({absolute})"
+
+@app.route("/dlplot.png")
+def dlplot_png():
+
+    selected_code = request.args.get('code_delegation')
+    sexe = request.args.get('sexe')
+    age = request.args.get('age')
+    if not age or age == "undefined":
+        age = None
+    if not sexe or sexe == "undefined":
+        sexe = None
+    print(selected_code,sexe,age)
+    if sexe=="both" and not age:
+        if not selected_code:
+            return "No Code_Gouvernorat provided", 400
+
+       
+        response = requests.get("https://moors.rf.gd/query.php", params={
+        "selected_code": selected_code,
+        "sexe":sexe
+
+    })
+
+        if response.status_code != 200:
+            return "Failed to fetch data", 500
+
+        data = response.json()
+        df = pd.DataFrame(data)
+        plt.figure(figsize=(6,6))
+        plt.pie(
+        df['total_population'],
+        labels=df['Sexe'],
+        autopct=lambda pct: autopct_format(pct, df['total_population']),
+        startangle=140
+        )
+        plt.axis('equal')
+
+        img = io.BytesIO()
+        plt.savefig(img, format='png')
+        plt.close()
+        img.seek(0)
+        return Response(img.getvalue(), mimetype='image/png')
+    elif sexe != "both" and not age:
+        if not selected_code:
+            return "No Code_Gouvernorat provided", 400
+
+        response = requests.get("https://moors.rf.gd/query.php", params={
+        "selected_code": selected_code,
+        "sexe":sexe,
+    })
+
+        if response.status_code != 200:
+            return "Failed to fetch data", 500
+
+        data = response.json()
+        df = pd.DataFrame(data)
+
+        if df.empty:
+            return "No data found for the selected delegation and sex.", 404
+
+        plt.figure(figsize=(6, 6))
+        plt.bar(df['Sexe'], df['total_population'], color='skyblue')
+        plt.xlabel('Sexe')
+        plt.ylabel('Population')
+        plt.title(f"Population for Sex: {df.iloc[0]['Sexe']} in Delegation {df['total_population'][0]}")
+    
+        img = io.BytesIO()
+        plt.savefig(img, format='png')
+        plt.close()
+        img.seek(0)
+        return Response(img.getvalue(), mimetype='image/png')
+    elif sexe == "both" and age=="age":
+        if not selected_code:
+            return "No Code_Gouvernorat provided", 400
+
+        response = requests.get("https://moors.rf.gd/query.php", params={
+        "selected_code": "selected_code",
+        "sexe":sexe,
+        "age":age
+        })
+
+        if response.status_code != 200:
+            return "Failed to fetch data", 500
+
+        data = response.json()
+        df = pd.DataFrame(data)
+
+        if df.empty:
+            return "No data found for the selected delegation and sex.", 404
+
+        plt.figure(figsize=(20,20))
+        df['label'] = df['Sexe'] + " (" + df['Trancheage'] + ")\n" + df['total_population'].astype(str)
+        plt.pie(
+
+        df['total_population'],
+        labels=df['label'],
+        autopct=lambda pct: autopct_format(pct,''),
+        startangle=140
+        )
+        plt.axis('equal')
+
+        img = io.BytesIO()
+        plt.savefig(img, format='png')
+        plt.close()
+        img.seek(0)
+        return Response(img.getvalue(), mimetype='image/png')
+    elif sexe != "both" and age=="noage":
+        if not selected_code:
+            return "No Code_Gouvernorat provided", 400
+
+        response = requests.get("https://moors.rf.gd/query.php", params={
+        "selected_code": "selected_code",
+        "sexe":sexe,
+        "age" :age
+
+        })
+
+        if response.status_code != 200:
+            return "Failed to fetch data", 500
+
+        data = response.json()
+        df = pd.DataFrame(data)
+
+        if df.empty:
+            return "No data found for the selected delegation and sex.", 404
+
+
+     
+        plt.figure(figsize=(20,20))
+        df['label'] = df['Sexe'] +' ('+df['total_population'].astype(str)+')'
+        plt.pie(
+
+        df['total_population'],
+        labels=df['label'],
+        autopct=lambda pct: autopct_format(pct,''),
+        startangle=140
+        )
+        plt.axis('equal')
+
+        img = io.BytesIO()
+        plt.savefig(img, format='png')
+        plt.close()
+        img.seek(0)
+        return Response(img.getvalue(), mimetype='image/png')
+    elif sexe != "both" and age=="age":
+        if not selected_code:
+            return "No Code_Gouvernorat provided", 400
+
+        response = requests.get("https://moors.rf.gd/query.php", params={
+        "selected_code": "selected_code",
+        "sexe":sexe,
+        "age" :age
+
+        })
+
+        if response.status_code != 200:
+            return "Failed to fetch data", 500
+
+        data = response.json()
+        df = pd.DataFrame(data)
+
+        if df.empty:
+            return "No data found for the selected delegation and sex.", 404
+
+
+     
+        plt.figure(figsize=(20,20))
+        df['label'] = df['Sexe'] + " (" + df['Trancheage'] + ")\n" + df['total_population'].astype(str)
+        plt.pie(
+
+        df['total_population'],
+        labels=df['label'],
+        autopct=lambda pct: autopct_format(pct,''),
+        startangle=140
+        )
+        plt.axis('equal')
+
+        img = io.BytesIO()
+        plt.savefig(img, format='png')
+        plt.close()
+        img.seek(0)
+        return Response(img.getvalue(), mimetype='image/png')
+
+            
+
+    
+
+
+@app.route("/gvplot.png")
+def gvplot_png():
+    selected_code = request.args.get('code_gouvernorat')
+    sexe = request.args.get('sexe')
+    age = request.args.get('age')
+    if not age or age == "undefined":
+        age = None
+    if not sexe or sexe == "undefined":
+        sexe = None
+    if sexe=="both" and not age:
+        if not selected_code:
+            return "No Code_Gouvernorat provided", 400
+
+       
+        response = requests.get("https://moors.rf.gd/query.php", params={
+        "gouvernorats":"1",
+        "selected_code": "selected_code",
+        "sexe":sexe,
+        "age" :age
+
+        })
+
+        if response.status_code != 200:
+            return "Failed to fetch data", 500
+
+        data = response.json()
+        df = pd.DataFrame(data)
+
+        plt.figure(figsize=(6,6))
+        plt.pie(
+        df['total_population'],
+        labels=df['Sexe'],
+        autopct=lambda pct: autopct_format(pct, df['total_population']),
+        startangle=140
+        )
+        plt.axis('equal')
+
+        img = io.BytesIO()
+        plt.savefig(img, format='png')
+        plt.close()
+        img.seek(0)
+        return Response(img.getvalue(), mimetype='image/png')
+    elif sexe != "both" and not age:
+        if not selected_code:
+            return "No Code_Gouvernorat provided", 400
+
+        response = requests.get("https://moors.rf.gd/query.php", params={
+        "gouvernorats":"1",
+        "selected_code": "selected_code",
+        "sexe":sexe,
+        "age" :age
+
+        })
+
+        if response.status_code != 200:
+            return "Failed to fetch data", 500
+
+        data = response.json()
+        df = pd.DataFrame(data)
+
+        if df.empty:
+            return "No data found for the selected delegation and sex.", 404
+
+        plt.figure(figsize=(6, 6))
+        plt.bar(df['Sexe'], df['total_population'], color='skyblue')
+        plt.xlabel('Sexe')
+        plt.ylabel('Population')
+        plt.title(f"Population for Sex: {df.iloc[0]['Sexe']} in Gouvernorat {df['total_population'][0]}")
+    
+        img = io.BytesIO()
+        plt.savefig(img, format='png')
+        plt.close()
+        img.seek(0)
+        return Response(img.getvalue(), mimetype='image/png')
+    elif sexe == "both" and age=="age":
+        if not selected_code:
+            return "No Code_Gouvernorat provided", 400
+
+        response = requests.get("https://moors.rf.gd/query.php", params={
+        "gouvernorats":"1",
+        "selected_code": "selected_code",
+        "sexe":sexe,
+        "age" :age
+
+        })
+
+        if response.status_code != 200:
+            return "Failed to fetch data", 500
+
+        data = response.json()
+        df = pd.DataFrame(data)
+
+        if df.empty:
+            return "No data found for the selected delegation and sex.", 404
+
+        plt.figure(figsize=(20,20))
+        df['label'] = df['Sexe'] + " (" + df['Trancheage'] + ")\n" + df['total_population'].astype(str)
+        plt.pie(
+
+        df['total_population'],
+        labels=df['label'],
+        autopct=lambda pct: autopct_format(pct,''),
+        startangle=140
+        )
+        plt.axis('equal')
+
+        img = io.BytesIO()
+        plt.savefig(img, format='png')
+        plt.close()
+        img.seek(0)
+        return Response(img.getvalue(), mimetype='image/png')
+    elif sexe == "both" and age=="noage":
+        if not selected_code:
+            return "No Code_Gouvernorat provided", 400
+
+        response = requests.get("https://moors.rf.gd/query.php", params={
+        "gouvernorats":"1",
+        "selected_code": "selected_code",
+        "sexe":sexe,
+        "age" :age
+
+        })
+
+        if response.status_code != 200:
+            return "Failed to fetch data", 500
+
+        data = response.json()
+        df = pd.DataFrame(data)
+
+        if df.empty:
+            return "No data found for the selected delegation and sex.", 404
+
+        plt.figure(figsize=(20,20))
+        df['label'] = df['Sexe'] +' ('+df['total_population'].astype(str)+')'
+        plt.pie(
+
+        df['total_population'],
+        labels=df['label'],
+        autopct=lambda pct: autopct_format(pct,''),
+        startangle=140
+        )
+        plt.axis('equal')
+
+        img = io.BytesIO()
+        plt.savefig(img, format='png')
+        plt.close()
+        img.seek(0)
+        return Response(img.getvalue(), mimetype='image/png')
+    elif sexe != "both" and age=="age":
+        if not selected_code:
+            return "No Code_Gouvernorat provided", 400
+
+        response = requests.get("https://moors.rf.gd/query.php", params={
+        "gouvernorats":"1",
+        "selected_code": "selected_code",
+        "sexe":sexe,
+        "age" :age
+
+        })
+
+        if response.status_code != 200:
+            return "Failed to fetch data", 500
+
+        data = response.json()
+        df = pd.DataFrame(data)
+        if df.empty:
+            return "No data found for the selected delegation and sex.", 404
+
+
+     
+        plt.figure(figsize=(20,20))
+        df['label'] = df['Sexe'] + " (" + df['Trancheage'] + ")\n" + df['total_population'].astype(str)
+        plt.pie(
+
+        df['total_population'],
+        labels=df['label'],
+        autopct=lambda pct: autopct_format(pct,''),
+        startangle=140
+        )
+        plt.axis('equal')
+
+        img = io.BytesIO()
+        plt.savefig(img, format='png')
+        plt.close()
+        img.seek(0)
+        return Response(img.getvalue(), mimetype='image/png')
+    elif sexe != "both" and age=="noage":
+        if not selected_code:
+            return "No Code_Gouvernorat provided", 400
+
+        response = requests.get("https://moors.rf.gd/query.php", params={
+        "gouvernorats":"1",
+        "selected_code": "selected_code",
+        "sexe":sexe,
+        "age" :age
+
+        })
+
+        if response.status_code != 200:
+            return "Failed to fetch data", 500
+
+        data = response.json()
+        df = pd.DataFrame(data)
+        if df.empty:
+            return "No data found for the selected delegation and sex.", 404
+
+
+     
+        plt.figure(figsize=(20,20))
+        df['label'] = df['Sexe'] +' ('+df['total_population'].astype(str)+')'
+        plt.pie(
+
+        df['total_population'],
+        labels=df['label'],
+        autopct=lambda pct: autopct_format(pct,''),
+        startangle=140
+        )
+        plt.axis('equal')
+
+        img = io.BytesIO()
+        plt.savefig(img, format='png')
+        plt.close()
+        img.seek(0)
+        return Response(img.getvalue(), mimetype='image/png')
 
 
 
