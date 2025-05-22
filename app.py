d36cff868f55071251b2c9a22e393287cee76d14
Moorsknight11 <hajjejhazem063@gmail.com>
2025-05-22 02:53:00 +0200

    Fresh start

--- app.py
+++ app.py
@@ -523,9 +523,11 @@ username = os.getenv('DB_USERNAME')
 password = os.getenv('DB_PASSWORD')
 host = os.getenv('DB_HOST')
 database = os.getenv('DB_NAME')
+port = 17722
 
 
-engine = create_engine(f'mysql+pymysql://{username}:{password}@{host}/{database}')
+engine = create_engine(f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}')
+
 @app.route("/delegations")
 def get_delegations():
     code_gov = request.args.get("code_gouvernorat")
